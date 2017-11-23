b66b6ce29cfbdd767ea74ea60509c23a2a2c662b
Daniel <dfragoso@tasken.com>
2017-11-23 20:43:29 -0200

    funcões utilitárias para a conversão de tipos em vetores de bytes

--- utils/commonUtils.go
+++ utils/commonUtils.go
@@ -2,7 +2,9 @@ package utils
 
 import (
 	"bytes"
+	"encoding/binary"
 	"encoding/json"
+	"errors"
 	"math"
 	"math/rand"
 	"time"
@@ -42,4 +44,36 @@ func EncryptWithXor(payload, key, fillers []byte) []byte {
 		}
 		return result
 	}
+}
+
+//ConvertInt64ToByte parse int64 to byte slice
+func ConvertInt64ToByte(num int64) []byte {
+	intBuffer := make([]byte, binary.MaxVarintLen64)
+	length := binary.PutVarint(intBuffer, num)
+	return intBuffer[:length]
+} 
+
+//GetInt64FromByte parse slice to int64
+func GetInt64FromByte(b []byte) (int64, error) {
+	num, readedBytes := binary.Varint(b)
+	if readedBytes != len(b) {
+		return num, errors.New("The byte slice length does not match the int64 length")
+	}
+	return num, nil
+}
+
+//ConvertUint64ToByte parse uint64 to byte slice
+func ConvertUint64ToByte(num uint64) []byte {
+	intBuffer := make([]byte, binary.MaxVarintLen64)
+	length := binary.PutUvarint(intBuffer, num)
+	return intBuffer[:length]
+} 
+
+//GetUint64FromByte parse slice to uint64
+func GetUint64FromByte(b []byte) (uint64, error) {
+	num, readedBytes := binary.Uvarint(b)
+	if readedBytes != len(b) {
+		return num, errors.New("The byte slice length does not match the uint64 length")
+	}
+	return num, nil
 }
